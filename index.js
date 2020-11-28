537f406c6a323f3c3b67c10a19d1162d7b597e8f
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-11-27 16:16:54 -0800

    feat: initial version

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  schemas: {
+    ["api.github.com.deref"]: require("./generated/api.github.com.deref.json"),
+    ["api.github.com"]: require("./generated/api.github.com.json"),
+    ["ghes-2.18.deref"]: require("./generated/ghes-2.18.deref.json"),
+    ["ghes-2.18"]: require("./generated/ghes-2.18.json"),
+    ["ghes-2.19.deref"]: require("./generated/ghes-2.19.deref.json"),
+    ["ghes-2.19"]: require("./generated/ghes-2.19.json"),
+    ["ghes-2.20.deref"]: require("./generated/ghes-2.20.deref.json"),
+    ["ghes-2.20"]: require("./generated/ghes-2.20.json"),
+    ["ghes-2.21.deref"]: require("./generated/ghes-2.21.deref.json"),
+    ["ghes-2.21"]: require("./generated/ghes-2.21.json"),
+    ["ghes-2.22.deref"]: require("./generated/ghes-2.22.deref.json"),
+    ["ghes-2.22"]: require("./generated/ghes-2.22.json"),
+    ["github.ae.deref"]: require("./generated/github.ae.deref.json"),
+    ["github.ae"]: require("./generated/github.ae.json"),
+  },
+};
