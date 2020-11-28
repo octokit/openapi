4f3a79a981a5db04db4f905a4ac64c1719c1790e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2020-11-27 16:16:06 -0800

    build(scripts): initial version

---
+++ scripts/build.js
@@ -0,0 +1,65 @@
+const { readdirSync, writeFileSync } = require("fs");
+const prettier = require("prettier");
+
+const schemaFileNames = readdirSync("cache");
+const changeFileNames = readdirSync("changes");
+
+const changes = changeFileNames.reduce((map, file) => {
+  const { route, ...change } = require(`../changes/${file}`);
+  if (!map[route]) map[route] = [];
+  map[route].push(change);
+  return map;
+}, {});
+
+for (const file of schemaFileNames) {
+  const schema = require(`../cache/${file}`);
+
+  for (const [path, methods] of Object.entries(schema.paths)) {
+    for (const [method, operation] of Object.entries(methods)) {
+      const route = `${method.toUpperCase()} ${path}`;
+      operation["x-octokit"] = {};
+
+      if (!changes[route]) continue;
+
+      operation["x-octokit"].changes = [];
+      for (const change of changes[route]) {
+        operation["x-octokit"].changes.push(change);
+      }
+    }
+  }
+
+  // overwrite version to "0.0.0-development", will be updated
+  // right before publish via semantic-release
+  schema.info.version = "0.0.0-development";
+
+  writeFileSync(
+    `generated/${file}`,
+    prettier.format(JSON.stringify(schema), { parser: "json" })
+  );
+  console.log(`generated/${file} written`);
+}
+
+let schemasCode = "";
+
+for (const name of schemaFileNames) {
+  schemasCode += `["${name.replace(
+    ".json",
+    ""
+  )}"]: require("./generated/${name}"),`;
+}
+
+writeFileSync(
+  "index.js",
+  prettier.format(
+    `
+      module.exports = {
+        schemas: {
+          ${schemasCode}
+        }
+      }
+    `,
+    {
+      parser: "babel",
+    }
+  )
+);
