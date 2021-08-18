5d89f047dc64b3217c735c63c4c9eba59e9f3bb5
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-17 17:25:02 -0700

    build: build `generated/*.diff.json` files

--- scripts/build.js
+++ scripts/build.js
@@ -1,74 +1,137 @@
-const { readdirSync, writeFileSync } = require("fs");
+const { createWriteStream, readdirSync, writeFileSync } = require("fs");
+const { resolve } = require("path");
+
 const prettier = require("prettier");
+const execa = require("execa");
 
 const overrides = require("./overrides");
 
-const schemaFileNames = readdirSync("cache");
-const changeFileNames = readdirSync("changes");
+if (!process.env.ANICCA_REPOSITORY_PATH) {
+  throw new Error("Please set ANICCA_REPOSITORY_PATH environment variable");
+}
 
-const changes = changeFileNames.reduce((map, file) => {
-  const { route, ...change } = require(`../changes/${file}`);
-  if (!map[route]) map[route] = [];
-  map[route].push(change);
-  return map;
-}, {});
+run();
 
-for (const file of schemaFileNames) {
-  const schema = require(`../cache/${file}`);
+async function run() {
+  const schemaFileNames = readdirSync("cache");
+  const changeFileNames = readdirSync("changes");
 
-  // apply overrides to the unaltered schemas from GitHub
-  overrides(file, schema);
+  const changes = changeFileNames.reduce((map, file) => {
+    const { route, ...change } = require(`../changes/${file}`);
+    if (!map[route]) map[route] = [];
+    map[route].push(change);
+    return map;
+  }, {});
 
-  for (const [path, methods] of Object.entries(schema.paths)) {
-    for (const [method, operation] of Object.entries(methods)) {
-      const route = `${method.toUpperCase()} ${path}`;
-      operation["x-octokit"] = {};
+  for (const file of schemaFileNames) {
+    const schema = require(`../cache/${file}`);
 
-      if (!changes[route]) continue;
+    // apply overrides to the unaltered schemas from GitHub
+    overrides(file, schema);
 
-      operation["x-octokit"].changes = [];
-      for (const change of changes[route]) {
-        operation["x-octokit"].changes.push(change);
+    for (const [path, methods] of Object.entries(schema.paths)) {
+      for (const [method, operation] of Object.entries(methods)) {
+        const route = `${method.toUpperCase()} ${path}`;
+        operation["x-octokit"] = {};
+
+        if (!changes[route]) continue;
+
+        operation["x-octokit"].changes = [];
+        for (const change of changes[route]) {
+          operation["x-octokit"].changes.push(change);
+        }
       }
     }
+
+    // overwrite version to "0.0.0-development", will be updated
+    // right before publish via semantic-release
+    schema.info.version = "0.0.0-development";
+    schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
+    schema.info.description =
+      "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
+    schema.info.contact.url = "https://github.com/octokit/openapi";
+
+    writeFileSync(
+      `generated/${file}`,
+      prettier.format(JSON.stringify(schema), { parser: "json" })
+    );
+    console.log(`generated/${file} written`);
   }
 
-  // overwrite version to "0.0.0-development", will be updated
-  // right before publish via semantic-release
-  schema.info.version = "0.0.0-development";
-  schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
-  schema.info.description =
-    "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
-  schema.info.contact.url = "https://github.com/octokit/openapi";
+  // generate diff files
+  for (const file of schemaFileNames) {
+    if (!file.endsWith("deref.json")) continue;
+    if (file.startsWith("api.github.com")) continue;
+
+    const fromPath = `generated/${toFromFilename(file)}`;
+    const toPath = `generated/${file}`;
+    const diffPath = `generated/${file.replace(".deref.", ".diff.")}`;
+
+    const cmd = `cargo run --bin cli diff ${resolve(fromPath)} ${resolve(
+      toPath
+    )} --format json`;
+
+    console.log("$ %s", cmd);
+    // generate diff files using `anicca`
+    // cargo run --bin cli diff /Users/gregor/Projects/octokit/openapi/generated/api.github.com.deref.json /Users/gregor/Projects/octokit/openapi/generated/ghes-3.1.deref.json --format json > diff.json
+    const command = execa.command(cmd, {
+      cwd: process.env.ANICCA_REPOSITORY_PATH,
+    });
+    command.stderr.pipe(process.stderr);
+    command.stdout.pipe(createWriteStream(resolve(diffPath)));
+    await command;
+
+    console.log(`${diffPath} written`);
+  }
+
+  let schemasCode = "";
+
+  for (const name of schemaFileNames) {
+    schemasCode += `["${name.replace(
+      ".json",
+      ""
+    )}"]: require("./generated/${name}"),`;
+  }
 
   writeFileSync(
-    `generated/${file}`,
-    prettier.format(JSON.stringify(schema), { parser: "json" })
-  );
-  console.log(`generated/${file} written`);
-}
-
-let schemasCode = "";
-
-for (const name of schemaFileNames) {
-  schemasCode += `["${name.replace(
-    ".json",
-    ""
-  )}"]: require("./generated/${name}"),`;
-}
-
-writeFileSync(
-  "index.js",
-  prettier.format(
-    `
+    "index.js",
+    prettier.format(
+      `
       module.exports = {
         schemas: {
           ${schemasCode}
         }
       }
     `,
-    {
-      parser: "babel",
-    }
-  )
-);
+      {
+        parser: "babel",
+      }
+    )
+  );
+}
+
+function toFromFilename(filename) {
+  if (filename.startsWith("github.ae")) {
+    return "api.github.com.deref.json";
+  }
+
+  if (filename.startsWith("ghes-3.1")) {
+    return "api.github.com.deref.json";
+  }
+
+  if (filename.startsWith("ghes-2.22")) {
+    return "ghes-3.0.deref.json";
+  }
+
+  if (filename.startsWith("ghes-3.")) {
+    const v3Version = parseInt(filename.substr("ghes-3.".length));
+    return `ghes-3.${v3Version + 1}.deref.json`;
+  }
+
+  if (filename.startsWith("ghes-2.")) {
+    const v2Version = parseInt(filename.substr("ghes-2.".length));
+    return `ghes-2.${v2Version + 1}.deref.json`;
+  }
+
+  throw new Error(`Cannot calculate base version for ${filename}`);
+}
