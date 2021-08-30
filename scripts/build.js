6b4024ab45826c7a099f789f5e92dcbdf8df2b5a
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-30 16:20:30 -0700

    build: reduce diff files (#152)

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,11 @@ const { resolve } = require("path");
 const prettier = require("prettier");
 const execa = require("execa");
 const sortKeys = require("sort-keys");
+const equal = require("deep-equal");
+const _ = require("lodash");
 
 const overrides = require("./overrides");
+const mapObj = require("./lib/map-obj");
 
 if (!process.env.ANICCA_REPOSITORY_PATH && !process.env.GITHUB_WORKSPACE) {
   throw new Error("Please set ANICCA_REPOSITORY_PATH or GITHUB_WORKSPACE");
@@ -24,40 +27,40 @@ async function run() {
     return map;
   }, {});
 
-  for (const file of schemaFileNames) {
-    const schema = require(`../cache/${file}`);
+  // for (const file of schemaFileNames) {
+  //   const schema = require(`../cache/${file}`);
 
-    // apply overrides to the unaltered schemas from GitHub
-    overrides(file, schema);
+  //   // apply overrides to the unaltered schemas from GitHub
+  //   overrides(file, schema);
 
-    for (const [path, methods] of Object.entries(schema.paths)) {
-      for (const [method, operation] of Object.entries(methods)) {
-        const route = `${method.toUpperCase()} ${path}`;
-        operation["x-octokit"] = {};
+  //   for (const [path, methods] of Object.entries(schema.paths)) {
+  //     for (const [method, operation] of Object.entries(methods)) {
+  //       const route = `${method.toUpperCase()} ${path}`;
+  //       operation["x-octokit"] = {};
 
-        if (!changes[route]) continue;
+  //       if (!changes[route]) continue;
 
-        operation["x-octokit"].changes = [];
-        for (const change of changes[route]) {
-          operation["x-octokit"].changes.push(change);
-        }
-      }
-    }
+  //       operation["x-octokit"].changes = [];
+  //       for (const change of changes[route]) {
+  //         operation["x-octokit"].changes.push(change);
+  //       }
+  //     }
+  //   }
 
-    // overwrite version to "0.0.0-development", will be updated
-    // right before publish via semantic-release
-    schema.info.version = "0.0.0-development";
-    schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
-    schema.info.description =
-      "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
-    schema.info.contact.url = "https://github.com/octokit/openapi";
+  //   // overwrite version to "0.0.0-development", will be updated
+  //   // right before publish via semantic-release
+  //   schema.info.version = "0.0.0-development";
+  //   schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
+  //   schema.info.description =
+  //     "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
+  //   schema.info.contact.url = "https://github.com/octokit/openapi";
 
-    writeFileSync(
-      `generated/${file}`,
-      prettier.format(JSON.stringify(schema), { parser: "json" })
-    );
-    console.log(`generated/${file} written`);
-  }
+  //   writeFileSync(
+  //     `generated/${file}`,
+  //     prettier.format(JSON.stringify(schema), { parser: "json" })
+  //   );
+  //   console.log(`generated/${file} written`);
+  // }
 
   // generate diff files
   for (const file of schemaFileNames) {
@@ -89,14 +92,36 @@ async function run() {
     console.log(`${diffPath} written`);
 
     const json = require(`../${diffPath}`);
+
+    const jsonWithoutNullValues = mapObj(json, removeNullValues);
+    const newJson = mapObj(jsonWithoutNullValues, simplifyRemovedArrays, {
+      deep: true,
+    });
+
+    if (Object.keys(newJson.paths.changed).length) {
+      newJson.paths.changed = mapObj(
+        newJson.paths.changed,
+        removeUnchangedKeys,
+        {
+          deep: true,
+        }
+      );
+    }
+
+    const minimalJson = mapObj(newJson, removeDeepEmptyObjects, {
+      deep: true,
+    });
+
+    const sortedJson = sortKeys(minimalJson, { deep: true });
+
     writeFileSync(
       diffPath,
-      prettier.format(JSON.stringify(sortKeys(json, { deep: true })), {
+      prettier.format(JSON.stringify(sortedJson), {
         parser: "json",
       })
     );
 
-    console.log(`${diffPath} re-formatted and keys sorted`);
+    console.log(`${diffPath} re-formatted, keys sorted, and simplified`);
   }
 
   let schemasCode = "";
@@ -154,4 +179,107 @@ function toFromFilename(filename) {
 function toDiffFilename(filename) {
   const fromFilename = toFromFilename(filename);
   return filename.replace(".deref.json", `.anicca-diff-to-${fromFilename}`);
+}
+
+function removeUnchangedKeys(key, value) {
+  if (value === null) {
+    return mapObj.mapObjSkip;
+  }
+
+  // we don't care about description changes
+  if (
+    (key === "description" || key === "description_changed") &&
+    typeof value === "object" &&
+    equal(Object.keys(value).sort(), ["from", "to"])
+  ) {
+    return mapObj.mapObjSkip;
+  }
+
+  // we also don't care about operation summary changes
+  if (
+    key === "summary" &&
+    typeof value === "object" &&
+    equal(Object.keys(value).sort(), ["from", "to"])
+  ) {
+    return mapObj.mapObjSkip;
+  }
+
+  if (equal(Object.keys(value).sort(), ["added", "changed", "removed"])) {
+    value.changed = mapObj(value.changed, removeEmptyObjects);
+  }
+
+  if (equal(value, { added: [], changed: {}, removed: [] })) {
+    return mapObj.mapObjSkip;
+  }
+
+  if (equal(value, { added: [], removed: [] })) {
+    return mapObj.mapObjSkip;
+  }
+
+  if (
+    equal(Object.keys(value).sort(), [
+      "operations_added",
+      "operations_changed",
+      "operations_removed",
+    ])
+  ) {
+    value.operations_changed = mapObj(
+      value.operations_changed,
+      removeEmptyObjects
+    );
+
+    if (
+      equal(value, {
+        operations_added: [],
+        operations_changed: {},
+        operations_removed: [],
+      })
+    ) {
+      return mapObj.mapObjSkip;
+    }
+  }
+
+  return [key, value];
+}
+
+function removeNullValues(key, value) {
+  if (value === null) {
+    return mapObj.mapObjSkip;
+  }
+
+  return [key, value];
+}
+
+function simplifyRemovedArrays(key, value) {
+  if (key !== "removed") return [key, value];
+
+  if (!Array.isArray(value) || !Array.isArray(value[0])) return [key, value];
+
+  return [key, value.map(([removedKey]) => removedKey)];
+}
+
+function removeEmptyObjects(key, value) {
+  if (equal(value, {})) {
+    return mapObj.mapObjSkip;
+  }
+
+  return [key, value];
+}
+
+function removeDeepEmptyObjects(key, value) {
+  if (isEmptyDeep(value)) {
+    return mapObj.mapObjSkip;
+  }
+
+  return [key, value];
+}
+
+function isEmptyDeep(obj) {
+  if (_.isObject(obj)) {
+    if (Object.keys(obj).length === 0) return true;
+    return _.every(_.map(obj, (v) => isEmptyDeep(v)));
+  } else if (_.isString(obj)) {
+    return !obj.length;
+  }
+  return false;
 }
