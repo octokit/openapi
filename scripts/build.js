f954088b788e4a3abb6fe48d0c88047c7fc4351e
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-08-31 10:14:57 -0700

    build: uncomment code to build specs for Octokit

--- scripts/build.js
+++ scripts/build.js
@@ -27,40 +27,40 @@ async function run() {
     return map;
   }, {});
 
-  // for (const file of schemaFileNames) {
-  //   const schema = require(`../cache/${file}`);
+  for (const file of schemaFileNames) {
+    const schema = require(`../cache/${file}`);
 
-  //   // apply overrides to the unaltered schemas from GitHub
-  //   overrides(file, schema);
+    // apply overrides to the unaltered schemas from GitHub
+    overrides(file, schema);
 
-  //   for (const [path, methods] of Object.entries(schema.paths)) {
-  //     for (const [method, operation] of Object.entries(methods)) {
-  //       const route = `${method.toUpperCase()} ${path}`;
-  //       operation["x-octokit"] = {};
+    for (const [path, methods] of Object.entries(schema.paths)) {
+      for (const [method, operation] of Object.entries(methods)) {
+        const route = `${method.toUpperCase()} ${path}`;
+        operation["x-octokit"] = {};
 
-  //       if (!changes[route]) continue;
+        if (!changes[route]) continue;
 
-  //       operation["x-octokit"].changes = [];
-  //       for (const change of changes[route]) {
-  //         operation["x-octokit"].changes.push(change);
-  //       }
-  //     }
-  //   }
+        operation["x-octokit"].changes = [];
+        for (const change of changes[route]) {
+          operation["x-octokit"].changes.push(change);
+        }
+      }
+    }
 
-  //   // overwrite version to "0.0.0-development", will be updated
-  //   // right before publish via semantic-release
-  //   schema.info.version = "0.0.0-development";
-  //   schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
-  //   schema.info.description =
-  //     "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
-  //   schema.info.contact.url = "https://github.com/octokit/openapi";
+    // overwrite version to "0.0.0-development", will be updated
+    // right before publish via semantic-release
+    schema.info.version = "0.0.0-development";
+    schema.info.title = "GitHub's official OpenAPI spec + Octokit extension";
+    schema.info.description =
+      "OpenAPI specs from https://github.com/github/rest-api-description with the 'x-octokit' extension required by the Octokit SDKs";
+    schema.info.contact.url = "https://github.com/octokit/openapi";
 
-  //   writeFileSync(
-  //     `generated/${file}`,
-  //     prettier.format(JSON.stringify(schema), { parser: "json" })
-  //   );
-  //   console.log(`generated/${file} written`);
-  // }
+    writeFileSync(
+      `generated/${file}`,
+      prettier.format(JSON.stringify(schema), { parser: "json" })
+    );
+    console.log(`generated/${file} written`);
+  }
 
   // generate diff files
   for (const file of schemaFileNames) {
