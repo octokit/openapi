15b09442a94658dd9f37ca2d1dc5e79674944c2f
Oscar Dominguez <dominguez.celada@gmail.com>
2021-10-03 15:30:17 +0200

    WIP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
     ["api.github.com.deref"]: require("./generated/api.github.com.deref.json"),
     ["api.github.com"]: require("./generated/api.github.com.json"),
     ["ghes-3.0.deref"]: require("./generated/ghes-3.0.deref.json"),
-    ["ghes-3.0"]: require("./generated/ghes-3.0.json"),
+    ["GitHub Enterprise Server 3.0"]: require("./generated/ghes-3.0.json"),
     ["ghes-3.1.deref"]: require("./generated/ghes-3.1.deref.json"),
     ["ghes-3.1"]: require("./generated/ghes-3.1.json"),
     ["ghes-3.2.deref"]: require("./generated/ghes-3.2.deref.json"),
