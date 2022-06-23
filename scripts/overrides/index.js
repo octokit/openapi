7e9db077ccd5360ea9431ce577865d0dfbd24a4b
Tim Rogers <me@timrogers.co.uk>
2022-06-23 20:45:15 +0100

    refactor: improve safety and correctness of overrides (#214)
    * build: refactor overrides to use shared helpers with helpful validations
    
    This refactors our OpenAPI overrides to use shared helpers
    (`rewriteOperationId`, `addOperation`) rather than bespoke code
    for each and every override.
    
    These helpers include simple validations - for example, if you
    try to change an operation ID and the operation doesn't exist,
    then it will error.
    
    This aims to increase the amount of code that is shared and
    enforce good hygiene - specifically, only having overrides that
    are valid and actually get applied. If you don't want to apply
    an override, then you can use an `if` condition rather than
    having the code as a no-op.
    
    * build: force a human to check overrides are valid when there is a new GHES version
    
    This updates the overrides script to explicitly state what GHES
    versions it supports.
    
    This means that, when there is a new GHES version, the build
    process will fail until a human looks at the code and checks that
    the provided overrides still make sense for the new version.
    
    This avoids applying overrides that don't make sense or having
    dead code hanging around for a long time.
    
    * fix: don't add non-existent `/repos/{owner}/{repo}/community/code_of_conduct` operation to Dotcom
    and AE

--- scripts/overrides/index.js
+++ scripts/overrides/index.js
@@ -1,10 +1,54 @@
+const SUPPORTED_GHES_OPERATIONS = [3.2, 3.3, 3.4, 3.5];
+
 module.exports = overrides;
+
+function isDeferenced(filename) {
+  return /deref/.test(filename);
+}
+
+// Updates the operation ID for a specific operation. Useful if you want to maintain
+// the function name in `plugin-rest-endpoint-methods.js` when the operation ID has
+// been changed in the OpenAPI specification.
+//
+// Throws an error if an operation is not found for the specified path and HTTP method.
+function rewriteOperationId(schema, path, httpMethod, operationId) {
+  if (!schema.paths[path]) {
+    throw `Path ${path} found not found in schema`;
+  }
+
+  if (!schema.paths[path][httpMethod]) {
+    throw `HTTP method ${httpMethod} not found for path ${path} in schema`;
+  }
+  
+  schema.paths[path][httpMethod].operationId = operationId;
+}
+
+// Adds an operation to the OpenAPI specification using JSON data stored in a file.
+//
+// Throws an error if an operation already exists for the specified path and HTTP method.
+function addOperation(schema, path, httpMethod, overridePath) {
+  if (schema.paths[path] && schema.paths[path][httpMethod]) {
+    throw `Operation \`${httpMethod} ${path}\` already exists`;
+  }
+
+  if (!schema.paths[path]) {
+    schema.paths[path] = {};
+  }
+
+  schema.paths[path][httpMethod] = require(overridePath);
+}
 
 function overrides(file, schema) {
   const isGHES = file.startsWith("ghes-");
   const ghesVersion = isGHES
     ? Number(file.match(/(?<=^ghes-)\d\.\d/)[0])
     : null;
+
+  if (isGHES && SUPPORTED_GHES_OPERATIONS.indexOf(ghesVersion) !== -1) {
+    throw `GHES version ${ghesVersion} is not yet supported. Please check the overrides ` +
+          `in \`scripts/overrides/index.js\` to check if they are relevant to this version, ` +
+          `and then update \`SUPPORTED_GHES_VERSION\`.`;
+  }  
 
   // remove `{ "type": "array", ...}` entries from `requestBody.content["aplication/json"].schema.anyOf
   // Octokit requires the request body to be set to an object in order to derive the variable name
@@ -30,66 +74,31 @@ function overrides(file, schema) {
     }
   }
 
-  // "/repos/{owner}/{repo}/compare/{base}...{head}" -> "/repos/{owner}/{repo}/compare/{basehead}"
-  if (schema.paths["/repos/{owner}/{repo}/compare/{basehead}"]) {
-    // update operation ID for new endpoint
-    schema.paths["/repos/{owner}/{repo}/compare/{basehead}"].get.operationId =
-      "repos/compare-commits-with-basehead";
+  rewriteOperationId(schema, "/repos/{owner}/{repo}/compare/{basehead}", 'get', 'repos/compare-commits-with-basehead')
 
-    // recove all endpoints
-    schema.paths["/repos/{owner}/{repo}/compare/{base}...{head}"] = {
-      get: /deref/.test(file)
-        ? require("./repos-compare-commits.deref.json")
-        : require("./repos-compare-commits.json"),
-    };
+  if (isDeferenced(file)) {
+    addOperation(schema, "/repos/{owner}/{repo}/compare/{base}...{head}", "get", "./repos-compare-commits.deref.json");
+  } else {
+    addOperation(schema, "/repos/{owner}/{repo}/compare/{base}...{head}", "get", "./repos-compare-commits.json");
   }
 
-  if (
-    schema.paths[
-      "/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-    ]
-  ) {
-    schema.paths[
-      "/repos/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-    ] =
-      schema.paths[
-        "/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-      ];
-    delete schema.paths[
-      "/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-    ];
+  if (ghesVersion === 3.2 || ghesVersion === 3.3) {
+    console.log('Doing 3.2 and 3.3 content ref rewrites');
+
+    rewriteOperationId(schema, "/repos/{owner}/{repo}/content_references/{content_reference_id}/attachments", "post", "apps/create-content-attachment-for-repo");
+
+    if (isDeferenced(file)) {
+      addOperation(schema, "/content_references/{content_reference_id}/attachments", "post", "./apps-create-content-attachment.deref.json");
+    } else {
+      addOperation(schema, "/content_references/{content_reference_id}/attachments", "post", "./apps-create-content-attachment.json")
+    }
   }
 
-  // "POST /content_references/{content_reference_id}/attachments" -> "POST /{owner}/{repo}/content_references/{content_reference_id}/attachments"
-  if (
-    schema.paths[
-      "/repos/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-    ]
-  ) {
-    // update operation ID for new endpoint
-    schema.paths[
-      "/repos/{owner}/{repo}/content_references/{content_reference_id}/attachments"
-    ].post.operationId = "apps/create-content-attachment-for-repo";
-
-    // recover all endpoints
-    schema.paths["/content_references/{content_reference_id}/attachments"] = {
-      post: /deref/.test(file)
-        ? require("./apps-create-content-attachment.deref.json")
-        : require("./apps-create-content-attachment.json"),
-    };
-  }
-
-  if (
-    file.startsWith("api.github.com") ||
-    file.startsWith("github.ae") ||
-    file.startsWith("ghes-3.2") ||
-    file.startsWith("ghes-3.3")
-  ) {
-    // recover `GET /repos/{owner}/{repo}/community/code_of_conduct` (with deprecation flags)
-    schema.paths["/repos/{owner}/{repo}/community/code_of_conduct"] = {
-      get: /deref/.test(file)
-        ? require("./codes-of-conduct-get-for-repo.deref.json")
-        : require("./codes-of-conduct-get-for-repo.json"),
-    };
+  if (ghesVersion === 3.2 || ghesVersion === 3.3) {
+    if (isDeferenced(file)) {
+      addOperation(schema, "/repos/{owner}/{repo}/community/code_of_conduct", "get", "./codes-of-conduct-get-for-repo.deref.json");
+    } else {
+      addOperation(schema, "/repos/{owner}/{repo}/community/code_of_conduct", "get", "./codes-of-conduct-get-for-repo.json");
+    }
   }
 }
