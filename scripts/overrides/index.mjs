f762863fb4db487d295bfd268fd66a629c9ad37f
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-05-24 14:13:56 -0500

    Adds GHES 3.13 as a supported operation

--- scripts/overrides/index.mjs
+++ scripts/overrides/index.mjs
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import { resolve, join, dirname } from "path";
 import { fileURLToPath } from "url";
 
-const SUPPORTED_GHES_OPERATIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"];
+const SUPPORTED_GHES_OPERATIONS = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"];
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function isDeferenced(filename) {
